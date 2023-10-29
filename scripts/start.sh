a50a0797b7033718962ec11c5a31671517600736
Dominique <dominique@kedokagroup.com>
2023-10-29 20:01:56 +0200

    Cleanup old workflows

--- scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(cd /home/ec2-user/Orniss-website ; docker-compose up -d --build)
--- scripts/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(cd /home/ec2-user/Orniss-website ; docker-compose down)
-sudo docker system prune --all --force
